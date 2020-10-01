8433f4625f396fe74d37a6df5b393f72ccf6735f
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-01 17:34:59 -0500

    Updates

---
+++ precache-manifest.5a8df42183dcb69f00105dbb0cb4caef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f0ee76db16c3ed364fa74a4780b1fe",
+    "url": "/barbosacap/index.html"
+  },
+  {
+    "revision": "7eb8ce2baf07acc26779",
+    "url": "/barbosacap/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2f624579ed5e582645b",
+    "url": "/barbosacap/static/js/2.4d365a4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barbosacap/static/js/2.4d365a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb8ce2baf07acc26779",
+    "url": "/barbosacap/static/js/main.6b046e37.chunk.js"
+  },
+  {
+    "revision": "dd385a4027cef37c5026",
+    "url": "/barbosacap/static/js/runtime-main.64e81d47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/barbosacap/static/media/logo.5d5d9eef.svg"
+  }
+]);
