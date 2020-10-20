f216c7f3021daae0298010547992f07ca6343060
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-20 02:32:15 -0500

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <code>
-          Test FiraCode: ->
+          Test FiraCode: -> <-
         </code>
       </header>
     </div>
