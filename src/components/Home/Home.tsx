98c2030d3f3d0ffb5a158fd105e81e5dd8787a61
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-23 02:24:55 -0500

    Add component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.scss';
+
+const Home: React.FC = () => (
+  <div className="Home">
+    Coder 
+    <code>-&gt;</code>
+  </div>
+);
+
+export default Home;
