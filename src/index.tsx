adc7ef9c75df220e6332053d5e7ebf853819aeec
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-24 03:45:11 -0500

    Deploy Settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './config/i18n';
 
@@ -14,12 +14,12 @@ ReactDOM.render(
   <React.StrictMode>
     <Suspense fallback={null}>
       <I18nextProvider i18n={i18n}>
-        <BrowserRouter>
+        <Router>
           <Switch>
             <Route path='/:locale' component={App} />
             <Redirect to='/en' />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </I18nextProvider>
     </Suspense>
   </React.StrictMode>,
