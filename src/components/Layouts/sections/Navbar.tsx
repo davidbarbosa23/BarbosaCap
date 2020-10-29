078103bc0bf30a5b76d1c2369eeed05889cba687
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-29 18:57:30 -0500

    temp disable nav item

--- src/components/Layouts/sections/Navbar.tsx
+++ src/components/Layouts/sections/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: React.FC = () => {
     <nav className='navbar'>
       <ul>
         <NavItem to={`/${t('lang')}`} content={t('home.menu')} />
-        <NavItem to={`/${t('lang')}/projects`} content={t('projects.menu')} />
+        {/* <NavItem to={`/${t('lang')}/projects`} content={t('projects.menu')} /> */}
         <NavItem to={null} className={'lang-button'} icon={flags[t('lang')]}>
           <LangSelector />
         </NavItem>
