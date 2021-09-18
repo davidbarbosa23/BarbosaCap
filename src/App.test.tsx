52646aa1e91ada435da694f878b05d89d7a9f659
David Barbosa <davidbarbosa.dev@gmail.com>
2021-09-17 23:03:30 -0500

    Jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Suspense, ReactElement, FunctionComponent } from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { render, RenderOptions } from '@testing-library/react';
+import { I18nextProvider } from 'react-i18next';
+import { MemoryRouter } from 'react-router-dom';
+
+import i18n from 'config/I18n';
 
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const Wrapper: FunctionComponent = ({ children }) => {
+  return (
+    <Suspense fallback={null}>
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+    </Suspense>
+  );
+};
+
+// create a customRender that wraps the UI in a memory Router
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: Wrapper, ...options });
+
+const routeComponentPropsMock = {
+  history: {} as any,
+  location: {} as any,
+  match: { isExact: true, params: { loacale: 'en' }, path: '', url: '' } as any,
+};
+
+test('renders react app', () => {
+  customRender(<App {...routeComponentPropsMock} />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
