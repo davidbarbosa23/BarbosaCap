62b264fe621e52a11d23606cb1ce289498c772e4
David Barbosa <davidbarbosa.dev@gmail.com>
2021-09-18 00:19:14 -0500

    starts release

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,26 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
-import { Redirect, Route, RouteComponentProps, Switch, useParams } from 'react-router-dom';
-
-import AppLangs, { defaultLang } from 'config/AppLangs';
-import { NotFound, Routes } from 'config/Routes';
-
-import { CurrentPathProvider } from 'contexts/currentPath';
-
-interface IParams {
-  locale: 'en' | 'es';
-}
-
-const App: React.FC<RouteComponentProps> = (props) => {
-  const { i18n } = useTranslation();
-  const { locale }: IParams = useParams();
-
-  useEffect(() => {
-    i18n.changeLanguage(locale ?? defaultLang);
-    // eslint-disable-next-line
-  }, [locale]);
-
+function App(): JSX.Element {
   return (
-    <>
-      <Helmet htmlAttributes={{ lang: locale ?? defaultLang }}>
-        <title>{process.env.REACT_APP_SITE_TITLE}</title>
-        <meta name="description" content={process.env.REACT_APP_SITE_DESC}></meta>
-      </Helmet>
-      <Switch>
-        {locale in AppLangs ? '' : <Redirect to={`/${defaultLang + '/' + locale}`} />}
-        {Routes(true).map(({ id, path, Component }: any) => (
-          <Route
-            key={id}
-            path={`${props.match.url}${locale in path ? path[locale] : path.en}`}
-            exact
-          >
-            <CurrentPathProvider path={{ id, path, lang: locale }}>
-              <Component />
-            </CurrentPathProvider>
-          </Route>
-        ))}
-        <Route component={NotFound} />
-      </Switch>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
