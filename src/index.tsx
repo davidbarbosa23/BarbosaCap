1945e6dbd4fdb84e4e6398a8fbe29470dccf92ad
David Barbosa <davidbarbosa.dev@gmail.com>
2021-09-19 01:21:32 -0500

    Exclude config from test suite

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './config/reportWebVitals';
 
 ReactDOM.render(
   <StrictMode>
